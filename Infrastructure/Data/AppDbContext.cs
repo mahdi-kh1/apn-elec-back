ba2a7c9e7309019b960f6f9b586fcebb3043420b
mahdi khodaee <mikhodaee@gmail.com>
2024-10-04 20:11:47 +0330

    2

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    DbSet<SysUser>  Users { get; set; }
+}
